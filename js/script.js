5383f4c42ea70687d8487f8fa15c713d8fc8d10f
Sanjay Kumar Singh01 <sanjay.singh01@irissoftware.com>
2019-11-06 10:03:54 +0530

    responsive updated

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.padding = "15px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.padding = "0";
+  }
+  
