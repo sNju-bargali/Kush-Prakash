5babd0bb62a43637cd1a79d6b456908414c27146
Sanjay Kumar Singh01 <sanjay.singh01@irissoftware.com>
2019-11-16 23:21:59 +0530

    slider and steps frform  update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenav").style.padding = "15px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("mySidenav").style.padding = "0";
-  }
-
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").style.padding = "15px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").style.padding = "0";
+}
